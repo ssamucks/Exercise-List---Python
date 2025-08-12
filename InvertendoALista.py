77a2d610f6eb583d4fa1756433fefc6b8ea1ffd9
Samuel Oliveira <samuel.kksd@gmail.com>
2025-08-11 22:16:14 -0300

    Solução Concluída do InvertendoALista

---
+++ InvertendoALista.py
@@ -0,0 +1,8 @@
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def inverterLista(lista):
+    listaInvertida = lista[::-1]
+    return listaInvertida
+
+print ("Este código diz respeito à análise de uma lista pré-estabelecida, com o objetivo de invertê-la.\n")
+print (f"A lista invertida ficou: {inverterLista(numeros)}")
